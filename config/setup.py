38099e6a8b99a2cc8c505ebc42ceb9f40be43c5a
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-21 17:40:44 +0700

    Initial commit

---
+++ config/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pyspark-wrapper",
+    version="0.0.1",
+    author="expecc",
+    author_email="expecc@expecc.com",
+    description="Common package for managing spark session",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
